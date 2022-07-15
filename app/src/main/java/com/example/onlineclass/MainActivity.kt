9428ebe0a4505ecfb8b2a0fb75d29ff70a650f61
2Kelvin <kvin.mwangi254@gmail.com>
2022-07-15 14:40:21 +0300

    added rounded corners to the image

--- app/src/main/java/com/example/onlineclass/MainActivity.kt
+++ app/src/main/java/com/example/onlineclass/MainActivity.kt
@@ -1,15 +1,30 @@
 package com.example.onlineclass
 
 import android.os.Bundle
+import android.view.RoundedCorner
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Search
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.onlineclass.ui.theme.OnlineClassTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,7 +46,37 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun OnlineClassApp() {
-
+    Column(
+        Modifier
+            .fillMaxSize()
+            .padding(all = 5.dp)
+            .background(Color.White)
+    ) {
+        // first row with profile
+        Row(
+            Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                imageVector = Icons.Outlined.Home,
+                contentDescription = null,
+                Modifier.weight(1f)
+            )
+            Image(
+                painter = painterResource(R.drawable.img),
+                contentDescription = "Profile",
+                Modifier
+                    .width(45.dp)
+                    .height(45.dp)
+                    .clip(RoundedCornerShape(50)) // image rounded corners
+            )
+            Icon(
+                imageVector = Icons.Outlined.Search,
+                contentDescription = null,
+                Modifier.weight(1f)
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
