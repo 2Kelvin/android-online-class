6360af935d376a404c953d206410a3610c8213f5
2Kelvin <kvin.mwangi254@gmail.com>
2022-07-15 19:07:49 +0300

    added and styled more composables

--- app/src/main/java/com/example/onlineclass/MainActivity.kt
+++ app/src/main/java/com/example/onlineclass/MainActivity.kt
@@ -10,13 +10,12 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material.icons.outlined.Search
+import androidx.compose.material.icons.outlined.Star
+import androidx.compose.material.icons.rounded.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -28,6 +27,7 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.onlineclass.ui.theme.OnlineClassTheme
 
 class MainActivity : ComponentActivity() {
@@ -62,9 +62,11 @@ fun OnlineClassApp() {
             .padding(all = 5.dp)
             .background(Color.White)
     ) {
-        // first row with profile
+        // first row with profile pic
         Row(
-            Modifier.fillMaxWidth(),
+            Modifier
+                .fillMaxWidth()
+                .padding(top = 10.dp),
             verticalAlignment = Alignment.CenterVertically
         ) {
             Icon(
@@ -89,8 +91,96 @@ fun OnlineClassApp() {
 
         Spacer(Modifier.height(10.dp))
 
-        // row Hello
+        // heading Hello
+        Text(
+            text = "Hello, Jessica",
+            fontSize = 40.sp,
+            modifier = Modifier.padding(top = 5.dp, bottom = 5.dp),
+            fontFamily = poppins
+        )
 
+        Spacer(Modifier.height(10.dp))
+
+        // row classes
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Text(
+                text = "Classes",
+                fontFamily = poppins,
+                fontWeight = FontWeight.Light,
+                modifier = Modifier.weight(2f),
+//                color = Color(0XFFE3E5E4)
+            )
+            Text(
+                text = "2 classes today",
+                fontFamily = poppins,
+                fontWeight = FontWeight.Light,
+                modifier = Modifier.weight(1f),
+//                color = Color(0XFFE3E5E4)
+            )
+        }
+
+        Spacer(Modifier.height(10.dp))
+
+        // row History
+        Row(
+            Modifier
+                .background(Color(0XFFE3E4E5))
+                .padding(all = 5.dp)
+                .clip(RoundedCornerShape(20))
+                .height(200.dp)
+                .fillMaxWidth()
+        ) {
+            Column(Modifier.weight(1f)) {
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Icon(
+                        imageVector = Icons.Rounded.Favorite,
+                        contentDescription = null,
+                        Modifier.weight(1f)
+                    )
+
+                    Column(Modifier.weight(2f)) {
+                        Text(
+                            text = "History",
+                            fontFamily = poppins
+                        )
+                        Text(
+                            text = "8:00 - 8:45",
+                            fontFamily = poppins,
+                            fontWeight = FontWeight.Light
+                        )
+                    }
+                }
+                Spacer(Modifier.height(10.dp))
+                Row(
+                    Modifier
+                        .background(Color.Black)
+                        .height(40.dp)
+                        .width(150.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(
+                        imageVector = Icons.Outlined.Star,
+                        contentDescription = null,
+//                        Modifier.weight(1f),
+                        tint = Color.White
+                    )
+//                    Divider(
+//                        Modifier.height(10.dp),
+//                        Color.White
+//                    )
+                    Text(
+                        text = "Join Now",
+                        fontFamily = poppins,
+                        modifier = Modifier.weight(1f),
+                        color = Color.White,
+
+                    )
+                }
+            }
+            Column(Modifier.weight(1f)) {
+
+            }
+        }
     }
 }
 
